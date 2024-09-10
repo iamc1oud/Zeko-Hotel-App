48edc331782e3a46568c64e4aa9844cfa5ba4eac
Ajay Kumar Singh <ajaykumar.kumar900@gmail.com>
2024-09-10 20:05:59 +0530

    added: notification action buttons

--- lib/features/home_screen/screens/bottom_navigation_bar.dart
+++ lib/features/home_screen/screens/bottom_navigation_bar.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:zeko_hotel_crm/features/analytics/screens/analytics_screens.dart';
 import 'package:zeko_hotel_crm/features/home_screen/screens/home_page_drawer.dart';
 import 'package:zeko_hotel_crm/features/order_management/screens/order_management_screens.dart';
@@ -19,18 +20,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   var _currentPage = 0;
 
+  final FlutterLocalNotificationsPlugin orderNotificationPlugin =
+      FlutterLocalNotificationsPlugin();
+
   @override
   void initState() {
     context.read<AuthCubit>().getHotelDetails();
 
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('launch_background');
+
+    const initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    orderNotificationPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: handleNotificationAction);
+
     // Foreground notification
     FirebaseMessaging.onMessage.listen((message) {
       if (message.notification != null) {
-        logger.d(
-            'Message also contained a notification: ${message.notification?.toMap()}');
+        orderNotificationPlugin.show(
+            message.notification.hashCode,
+            message.notification?.title,
+            message.notification?.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(channel.id, channel.name,
+                    icon: 'launch_background',
+                    colorized: true,
+                    actions: [
+                  const AndroidNotificationAction('accept_order', 'Accept',
+                      showsUserInterface: true, titleColor: Colors.green),
+                  const AndroidNotificationAction('reject_order', 'Reject',
+                      showsUserInterface: true)
+                ])));
       }
     });
     super.initState();
+  }
+
+  void handleNotificationAction(
+      NotificationResponse notificationResponse) async {
+    final String? actionId = notificationResponse.actionId;
+
+    if (actionId == 'accept_order') {
+      // Call the API when the "Accept" button is clicked
+      logger.d("Accept Order API");
+    } else if (actionId == 'reject_order') {
+      // Call the API when the "Reject" button is clicked
+      logger.d("Reject Order API");
+    }
   }
 
   final tabs = [
--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dropdown_alert/dropdown_alert.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:scaled_app/scaled_app.dart';
 import 'package:zeko_hotel_crm/core/core.dart';
 import 'package:zeko_hotel_crm/features/auth/data/repository/auth_repository.dart';
@@ -34,6 +36,79 @@ final logger = Logger();
 // Global service locator
 var getIt = GetIt.instance;
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
+}
+
+/// Create a [AndroidNotificationChannel] for heads up notifications
+late AndroidNotificationChannel channel;
+
+bool isFlutterLocalNotificationsInitialized = false;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  /// Create an Android Notification Channel.
+  ///
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
+}
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && !kIsWeb) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          // TODO add a proper drawable resource to android, for now using
+          //      one that already exists in example app.
+          icon: 'launch_background',
+        ),
+      ),
+    );
+  }
+}
+
 Future<void> main() async {
   ScaledWidgetsFlutterBinding.ensureInitialized(
     scaleFactor: (deviceSize) {
@@ -46,6 +121,12 @@ Future<void> main() async {
   // Register firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    await setupFlutterNotifications();
+  }
+
   /// Update the iOS foreground notification presentation options to allow
   /// heads up notifications.
   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
@@ -53,6 +134,9 @@ Future<void> main() async {
     badge: true,
     sound: true,
   );
+
+  // Ask for notification permission
+  await Permission.notification.request();
 
   // Load DI
   await injecteDependencies();
